aefbb14947c0d80eb112b8c8148df8ac6f750050
LPSEG1 <lpseg1@ulaval.ca>
2025-03-12 15:41:56 -0400

    Commit v.0.0.5
    - Changed the category button to a select with custom styling thanks to a JS script

--- js/app.js
+++ js/app.js
@@ -4,3 +4,71 @@ document.addEventListener("DOMContentLoaded", function() {
   var currentYear = new Date().getFullYear();
   document.getElementById('currentYear').textContent = currentYear;
 });
+
+/* Category custom select style script */
+
+var x, i, j, l, ll, selElmnt, a, b, c;
+x = document.getElementsByClassName("category-button");
+l = x.length;
+for (i = 0; i < l; i++) {
+  selElmnt = x[i].getElementsByTagName("select")[0];
+  ll = selElmnt.length;
+  a = document.createElement("div");
+  a.setAttribute("class", "category-selected");
+  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+  x[i].appendChild(a);
+  b = document.createElement("div");
+  b.setAttribute("class", "category-items category-hide");
+  for (j = 1; j < ll; j++) {
+    c = document.createElement("div");
+    c.innerHTML = selElmnt.options[j].innerHTML;
+    c.addEventListener("click", function(e) {
+        var y, i, k, s, h, sl, yl;
+        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+        sl = s.length;
+        h = this.parentNode.previousSibling;
+        for (i = 0; i < sl; i++) {
+          if (s.options[i].innerHTML == this.innerHTML) {
+            s.selectedIndex = i;
+            h.innerHTML = this.innerHTML;
+            y = this.parentNode.getElementsByClassName("already-selected");
+            yl = y.length;
+            for (k = 0; k < yl; k++) {
+              y[k].removeAttribute("class");
+            }
+            this.setAttribute("class", "already-selected");
+            break;
+          }
+        }
+        h.click();
+    });
+    b.appendChild(c);
+  }
+  x[i].appendChild(b);
+  a.addEventListener("click", function(e) {
+      e.stopPropagation();
+      closeAllSelect(this);
+      this.nextSibling.classList.toggle("category-hide");
+      this.classList.toggle("select-arrow-active");
+    });
+}
+function closeAllSelect(elmnt) {
+  var x, y, i, xl, yl, arrNo = [];
+  x = document.getElementsByClassName("category-items");
+  y = document.getElementsByClassName("category-selected");
+  xl = x.length;
+  yl = y.length;
+  for (i = 0; i < yl; i++) {
+    if (elmnt == y[i]) {
+      arrNo.push(i)
+    } else {
+      y[i].classList.remove("select-arrow-active");
+    }
+  }
+  for (i = 0; i < xl; i++) {
+    if (arrNo.indexOf(i)) {
+      x[i].classList.add("category-hide");
+    }
+  }
+}
+document.addEventListener("click", closeAllSelect);
