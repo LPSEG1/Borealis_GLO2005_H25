5a463a26734f8e69240bd2e29850c91eb8dc00ea
LPSEG1 <lpseg1@ulaval.ca>
2025-04-04 16:42:24 -0400

    Commit v0.2.1
    - Modified app.py to remove print (no longer necessary)

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, request
-from dotenv import load_dotenv
 import pymysql, pymysql.cursors
-import os, hashlib, pymysql
 import util
 
 app = Flask(__name__)
@@ -31,7 +29,6 @@ def search():
       cur.execute('SELECT DISTINCT P.nom_prod AS name, F.nom_four AS brand, P.description_prod AS description, P.prix_prod AS price, P.image_prod AS image, P.categorie_prod FROM produits P INNER JOIN fournisseurs F ON P.fid = F.fid WHERE P. categorie_prod = '+itemCategory+' AND P. nom_prod LIKE "%'+searchTerm+'%" OR F.nom_four LIKE "%'+searchTerm+'%" AND P.unite_prod > 0')
     items = cur.fetchall()
     util.connection_database().close()
-    print(items)
 
     return render_template('search.html', nbrItems = len(items), term=searchTerm, items=items)
   except Exception as e:
